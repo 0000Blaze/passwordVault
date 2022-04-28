71da444bd75d362f053cc1fa573c24f25fd0868b
0000Blaze <rchhetry5@gmail.com>
2022-04-28 14:54:16 +0545

    encryption made more secure

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-import functools
-from readline import insert_text
 import sqlite3,hashlib
 from tkinter import *
 from tkinter import simpledialog
 from functools import partial
+import uuid
+import pyperclip
+import base64
+import os
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.fernet import Fernet
 
-from pandas import array
+backend = default_backend()
+salt =b'2444'
+
+kdf=PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=backend
+)
+
+encryptionKey = 0
+
+def encrypt(message:bytes, key:bytes) -> bytes:
+    return Fernet(key).encrypt(message)
+
+def decrypt(message:bytes,token:bytes)->bytes:
+    return Fernet(token).decrypt(message)
 
 #Database
 with sqlite3.connect("password_vault.db") as db:
@@ -13,7 +36,8 @@ with sqlite3.connect("password_vault.db") as db:
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS masterpassword(
 id INTEGER PRIMARY KEY,
-password TEXT NOT NULL);
+password TEXT NOT NULL,
+recoveryKey TEXT NOT NULL);
 """)
 
 cursor.execute("""
@@ -32,16 +56,19 @@ def popUp(text):
 
 #Initiate Window
 window =Tk()
+window.update()
 window.title("Password Vault")
 
 def hashPassword(input):
-    hash=hashlib.md5(input)
-    hash=hash.hexdigest()
-    return hash
+    hash1 = hashlib.sha256(input)
+    hash1 = hash1.hexdigest()
+    return hash1
 
 def firstScreen():
-    window.geometry("200x150")
-    
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    window.geometry("250x150")
     lbl = Label(window, text="Create Master Password")
     lbl.config(anchor=CENTER)
     lbl.pack()
@@ -60,15 +87,23 @@ def firstScreen():
     lbl2.pack()
 
     def savePassword():
-        if txt.get() == txt1.get():
+        if txt.get() == txt1.get(): 
+            sql ="DELETE from masterpassword WHERE id = 1"
+            cursor.execute(sql)
+            
             hashedPassword = hashPassword(txt.get().encode('utf-8'))
-            # print(hashedPassword)
-            insert_password="""INSERT INTO masterpassword(password)
-            VALUES(?)"""
-            cursor.execute(insert_password,[(hashedPassword)])
+            key = str(uuid.uuid4().hex)
+            recoveryKey = hashPassword(key.encode('utf-8'))
+
+            global encryptionKey
+            encryptionKey = base64.urlsafe_b64encode(kdf.derive(txt.get().encode()))
+
+            insert_password="""INSERT INTO masterpassword(password,recoveryKey)
+            VALUES(?,?)"""
+            cursor.execute(insert_password,[(hashedPassword),(recoveryKey)])
             db.commit()
 
-            passwordVault()
+            recoveryScreen(key)
         else:
             lbl2.config(text="Passwords do not match")
 
@@ -76,9 +111,71 @@ def firstScreen():
     btn = Button(window,text="Save",command=savePassword)
     btn.pack(pady=10)
 
+def recoveryScreen(key):
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    window.geometry("250x150")
+    
+    lbl = Label(window, text="Save this code in a secure location")
+    lbl.config(anchor=CENTER)
+    lbl.pack()
+
+    lbl1 = Label(window,text= key)
+    lbl1.config(anchor=CENTER)
+    lbl1.pack()
+    
+    def copyKey():
+        pyperclip.copy(lbl1.cget("text"))
+
+    btn = Button(window,text="Copy key",command=copyKey)
+    btn.pack(pady=5)
+
+    def done():
+        passwordVault()
+
+    btn = Button(window,text="Done",command=done)
+    btn.pack(pady=5)
+
+def resetScreen():
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    window.geometry("200x150")
+    
+    lbl = Label(window, text="Enter recovery key")
+    lbl.config(anchor=CENTER)
+    lbl.pack()
+
+    txt =Entry(window,width=20)
+    txt.pack()
+    txt.focus()
+
+    lbl1 = Label(window)
+    lbl1.config(anchor=CENTER)
+    lbl1.pack()
+    
+    def getRecoveryKey():
+        recoveryKeyCheck = hashPassword(str(txt.get()).encode('utf-8'))
+        cursor.execute("SELECT * FROM masterpassword WHERE id = 1 AND recoveryKey = ?",[(recoveryKeyCheck)])
+        return cursor.fetchall()
+
+    def checkRecoveryKey():
+        checked = getRecoveryKey()
+        if checked:
+            firstScreen()
+        else:
+            txt.delete(0,'end')
+            lbl1.config(text="Wrong Key")            
+
+    btn = Button(window,text="Check Key",command=checkRecoveryKey)
+    btn.pack(pady=5)
 
 def loginScreen():
-    window.geometry("250x150")
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    window.geometry("250x200")
     
     lbl = Label(window, text="Enter Master Password")
     lbl.config(anchor=CENTER)
@@ -93,21 +190,26 @@ def loginScreen():
 
     def getMasterPassword():
         checkHashedPassword = hashPassword(txt.get().encode('utf-8'))
+        global encryptionKey
+        encryptionKey = base64.urlsafe_b64encode(kdf.derive(txt.get().encode()))
         cursor.execute("SELECT * FROM masterpassword WHERE id = 1 AND password= ?",[(checkHashedPassword)])
-        # print(checkHashedPassword)
         return cursor.fetchall()
 
     def checkPassword():
-        match = getMasterPassword()
-        # print(match)
-        
+        match = getMasterPassword()        
         if match:
             passwordVault()
         else:
             txt.delete(0,'end')
             lbl1.config(text="Wrong password")
 
+    def resetPassword():
+        resetScreen()
+
     btn = Button(window,text="Submit",command=checkPassword)
+    btn.pack(pady=10)
+
+    btn = Button(window,text="Reset password",command=resetPassword)
     btn.pack(pady=10)
 
 def passwordVault():
@@ -119,9 +221,9 @@ def passwordVault():
         text2="Username"
         text3="Password"
 
-        website=popUp(text1)
-        username=popUp(text2)
-        password=popUp(text3)
+        website=encrypt(popUp(text1).encode(),encryptionKey)
+        username=encrypt(popUp(text2).encode(),encryptionKey)
+        password=encrypt(popUp(text3).encode(),encryptionKey)
 
         insert_fields ="""INSERT INTO vault(website,username,password)
         VALUES(?,?,?)"""
@@ -137,8 +239,9 @@ def passwordVault():
 
         passwordVault()
 
-    window.geometry("750x350")
+    window.geometry("750x550")
 
+    global encryptionKey
     lbl =Label(window,text="PASSWORD VAULT")
     lbl.grid(column=1)
 
@@ -157,16 +260,20 @@ def passwordVault():
         i = 0
         while TRUE:
             cursor.execute("SELECT * FROM vault")
-            array = cursor.fetchall()
+            array1 = cursor.fetchall()
+            # print(array1)
             
-            lbl1 = Label(window,text=(array[i][1]),font=("Helvetica",12))
+            if (len(array1) == 0):
+                break
+            
+            lbl1 = Label(window,text=(decrypt(array1[i][1],encryptionKey)),font=("Helvetica",12))
             lbl1.grid(column=0,row= i+3)
-            lbl2 = Label(window,text=(array[i][2]),font=("Helvetica",12))
+            lbl2 = Label(window,text=(decrypt(array1[i][2],encryptionKey)),font=("Helvetica",12))
             lbl2.grid(column=1,row= i+3)
-            lbl3 = Label(window,text=(array[i][3]),font=("Helvetica",12))
+            lbl3 = Label(window,text=(decrypt(array1[i][3],encryptionKey)),font=("Helvetica",12))
             lbl3.grid(column=2,row= i+3)
 
-            btn = Button(window,text="Delete",command=partial(removeEntry,array[i][0]))
+            btn = Button(window,text="Delete",command=partial(removeEntry,array1[i][0]))
             btn.grid(column=3,row=i+3,pady=10)
 
             i=i+1
@@ -182,4 +289,3 @@ else:
     firstScreen()
 
 window.mainloop()
-
